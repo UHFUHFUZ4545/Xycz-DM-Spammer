0915e460f241624151abb0658c52ce2ec8afaea4
FlyingUnicorn3 <120280432+UHFUHFUZ4545@users.noreply.github.com>
2022-12-23 13:20:14 +0100

    Add files via upload

---
+++ XyczSpammer.py
@@ -0,0 +1,70 @@
+import time
+import pyautogui as auto
+from time import sleep
+import colorama
+from colorama import Fore, Style
+colorama.init()
+
+#PLEASE DO NOT STEAL OR EDIT THIS CODE
+#YOU WILL GET IN BIG TROUBLE IF YOU TRYING TO SELL IT OR POST IT WITH YOUR NAME AND MY CODE
+
+def main():
+    
+    print()
+print('\033[31m')
+print("""
+                        ooooooo  ooooo #oooooo   ocooo #  .oooooo.   # oooooooooooo #
+                          `8888    d8'  # `888.   .8'  # d8P'  `Y8b  #d'""""""d888' #
+                            Y888..8P    #  `888. .8'   #888          #      .888P   #
+                              `8888'     #   `888.8'    #888          #     d888'    #
+                            .8PY888.    #    `888'     #888          #   .888P      #
+                            d8'  `888b   #     888      #`88b    ooo  #  d888'    .P #
+                         o888o  o88888o #    o888o     # `Y8bood8P'  #.8888888888P  #
+                                        #              #             #              #
+                                        #              #             #              #
+    discord: xycz#4747                   ##             ##            ##             ##
+               
+""")
+print(""""
+    github:""")
+print('''
+    https://github.com/UHFUHFUZ4545/Xycz-DM-Spammer''')
+print()
+print()
+choice = input("""
+    {1} Run Spammer
+                        :   """) 
+
+def choice1():
+    print('\033[31m')
+print('\033[31m')
+username = input("""      
+    USERNAME: """)
+
+
+if choice == "1":
+    print()
+print("""
+    PRESS {ENTER} TO START""")
+input()
+Sp = input("""
+    WHAT YOU WANNA SPAM TO THAT K!D?!: """)
+print()
+print('\033[32m' + 
+    'Loading Spammer...')
+sleep(2.0)
+print('\033[31m')
+Ac = input("""
+    PRESS {ENTER} TO BRING THIS KID MUTE HIS FXCKING PHONE!""")
+print()
+print('\033[32m' + 
+    'Loading phone destroyer...')
+sleep(2.0)
+print('\033[31m')
+while True:
+    auto.write(Sp)
+    auto.press('enter')
+    sleep(0.001)
+    print("GET SPAMMED BY",username,"and !={{\{XYCZ}/}}=!")
+else:
+    ('')
